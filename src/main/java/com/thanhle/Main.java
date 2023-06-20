6930b98d54c70a4f329912e1b92c0892d456555c
Thanh Le <thanhlevan0203@gmail.com>
2023-06-20 01:21:42 -0700

    create EmployeeDao and beans.xml

--- src/main/java/com/thanhle/Main.java
+++ src/main/java/com/thanhle/Main.java
@@ -1,7 +1,33 @@
 package com.thanhle;
 
+import com.thanhle.dao.EmployeeDao;
+import com.thanhle.domain.Employee;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        String[] beans = context.getBeanDefinitionNames();
+
+        for (String beanName : beans) {
+            System.out.println(beanName);
+        }
+
+        EmployeeDao employeeDao = (EmployeeDao) context.getBean("employeeDao");
+
+        Employee e = new Employee(3, "Thanh Le", "Software Engineer", 100000);
+
+        //employeeDao.save(e);
+        //employeeDao.save2(e);
+        //employeeDao.findAll();
+        //employeeDao.findById(3);
+        //employeeDao.updateById(3, "FullStack Engineer", 120000);
+        //employeeDao.deleteById(3);
+        employeeDao.findEmployee(50000);
+
+
     }
 }
